f1152eb7e55e2160a1fb966b0b62316cd4500c18
Luís Tovar <72473883+LuisTovar0@users.noreply.github.com>
2022-05-05 17:39:55 +0100

    using useState React hook instead of a component class

--- README.md
+++ README.md
@@ -6,7 +6,9 @@ Welcome to my GitHub profile!
 
 + Contact me on [LinkedIn 💼](https://linkedin.com/in/luis--tovar)
 
-<p align="center"> 
-  Visitor count<br>
-  <img src="https://profile-counter.glitch.me/LuisTovar0/count.svg"  alt=""/>
+<p>
+
+Visitor count<br/>
+<img src="https://profile-counter.glitch.me/LuisTovar0/count.svg"  alt=""/>
+
 </p>
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,31 +1,16 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import logo from '../logo.svg';
 import './App.css';
 
-interface AppProps {
+export default function App() {
+  const [state, setter] = useState({count: 0});
+
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo"/>
+      <p> The site isn't ready, but it's going to be cool </p>
+      <button onClick={() => setter({count: state.count + 1})}>
+        Click me {state.count} </button>
+    </div>
+  );
 }
-
-interface AppState {
-  count: number;
-}
-
-export default class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps | Readonly<AppProps>) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p> The site isn't ready, but it's going to be cool </p>
-        <button onClick={() => this.setState({count: this.state.count + 1})}>
-          Click me {this.state.count} </button>
-      </div>
-    );
-  }
-}
-
